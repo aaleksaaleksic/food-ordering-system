ba4cbf56ff8debcfc37de33e4a5f1780b4edd1e8
aaleksaaleksic <aaleksic9722rn@raf.rs>
2025-07-30 21:41:59 +0200

    Initialize Spring project

---
+++ backend/src/main/java/me/remontada/nwp_backend/NwpBackendApplication.java
@@ -0,0 +1,13 @@
+package me.remontada.nwp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NwpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NwpBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/me/remontada/nwp_backend/NwpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.remontada.nwp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NwpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
